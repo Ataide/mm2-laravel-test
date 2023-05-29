75a4c9656767f220e6d0685ca21c7113985eeea0
Ataide <ataide.bastos@gmail.com>
2023-05-29 14:07:01 -0300

    Instalado rabbitMq

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
+import { Container } from "@/Components/Container";
+import Paper from "@mui/material/Paper";
 
 export default function Dashboard({ auth }) {
   return (
@@ -13,13 +15,62 @@ export default function Dashboard({ auth }) {
     >
       <Head title="Dashboard" />
 
-      <div className="py-12">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900">You're logged in!</div>
+      <Container>
+        <section className="flex gap-5">
+          <Paper className="p-5 w-52 text-center">
+            <h1 className="uppercase text-gray-500 font-semibold">
+              Total de pontos:
+            </h1>
+            <div className="text-center text-4xl"> 8 </div>
+            <span className="text-sm text-gray-400">
+              Quantidade de registro do Banco de dados.
+            </span>
+          </Paper>
+          <Paper className="p-5 w-48 text-center">
+            <h1 className="uppercase text-gray-500 font-semibold">
+              Colaboradores:
+            </h1>
+            <div className="text-center text-4xl"> 8 </div>
+            <span className="text-sm text-gray-400">
+              Quantidade de registro do Banco de dados.
+            </span>
+          </Paper>
+          <Paper className="p-5 w-48 text-center">
+            <h1 className="uppercase text-gray-500 font-semibold">Escalas:</h1>
+            <div className="text-center text-4xl"> 8 </div>
+            <span className="text-sm text-gray-400">
+              Quantidade de registro do Banco de dados.
+            </span>
+          </Paper>
+
+          <Paper className="p-5 w-48 text-center">
+            <h1 className="uppercase text-gray-500 font-semibold">Fila:</h1>
+            <div className="text-center text-4xl"> 8 </div>
+            <span className="text-sm text-gray-400">
+              Quantidade de itens na fila.
+            </span>
+          </Paper>
+        </section>
+
+        <section>
+          <div>
+            <h1 className="text-3xl pt-5">Lista RabbitMQ</h1>
+            <p className="pt-5 text-zinc-600">
+              Aqui será apresentando a lista dos pontos que estão na fila do
+              RabbitMQ a serem processadas.
+            </p>
           </div>
-        </div>
-      </div>
+        </section>
+        <section className="max-w-7xl mx-auto">
+          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900">
+              <p>Grid com lista do rabbitMQ </p>
+            </div>
+          </div>
+        </section>
+      </Container>
+
+      <div className="py-12"></div>
     </AuthenticatedLayout>
   );
 }
